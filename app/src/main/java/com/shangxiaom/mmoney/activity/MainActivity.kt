d3879e904cc38255d1b1d04be95a5d68609056d7
ShangXiaoM <shangming150@163.com>
2018-07-23 11:22:53 +0800

    修改kotlin依赖；简化lambda语法

--- app/src/main/java/com/shangxiaom/mmoney/activity/MainActivity.kt
+++ app/src/main/java/com/shangxiaom/mmoney/activity/MainActivity.kt
@@ -10,9 +10,9 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button_profit.setOnClickListener({
+        button_profit.setOnClickListener {
             forMoney()
-        })
+        }
     }
 
     override fun init() {
